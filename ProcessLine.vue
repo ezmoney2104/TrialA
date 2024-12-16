557228d7f16be2de7d2ca9c0e214bf6ae4cfadbf
ezmoney2104 <48146739+ezmoney2104@users.noreply.github.com>
2024-12-16 17:19:52 +0800

    Create ProcessLine.vue

---
+++ ProcessLine.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid>
+    <v-row><common-label :item="productionLineTitle" /></v-row>
+    <v-row><common-table :item-table="TABLE_PROPERTIES"></common-table></v-row>
+  </v-container>
+</template>
+
+<script>
+import { PRODUCTION_LINE_LABEL } from '@/common/recipe/rLabels'
+import CommonLabel from '@/components/common/CommonLabel.vue'
+import CommonTable from '@/components/common/CommonTable.vue'
+import { PROUDUCT_LINE_TABLE } from '@/common/recipe/rTables'
+
+export default {
+  name: 'ProcessLine',
+  components: {
+    CommonLabel,
+    CommonTable,
+  },
+  data() {
+    return {
+      TABLE_PROPERTIES: { ...PROUDUCT_LINE_TABLE },
+      productionLineTitle: PRODUCTION_LINE_LABEL,
+    }
+  },
+}
+</script>
